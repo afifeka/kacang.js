978ce462839c01ca9d01dc092fb406bf0ab13f4e
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-19 05:11:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,18 +20,15 @@ bot.on("ready", async () => {
 bot.on('guildMemberAdd', member => {
        
    const log = bot.channels.get("435341810248712192")
-   const log2 = member.guild.channels.get(`name`, "member-log");
+ 
    log.send(`Welcome, <@${member.author.tag}> In Server **${member.guild.name}**, Enjoy The Server And Dont Forget To Read The Rules! `);
-   log2.send(`Welcome, <@${member.author.tag}> In Server **${member.guild.name}**, Enjoy The Server And Dont Forget To Read The Rules! `);
-	
+   
 });
 bot.on('guildMemberRemove', member => {
        
    const log = bot.channels.get("435341810248712192")
-   const log2 = member.guild.channels.get(`name`, "member-log");
 
    log.send(`GoodBye, <@${member.author.tag}> In Server **${member.guild.name}**, See You Later!  `);
-   log2.send(`GoodBye, <@${member.author.tag}> In Server **${member.guild.name}**, See You Later!  `);
 
 });
 
