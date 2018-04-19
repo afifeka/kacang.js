94a8032dded98213c68b5724cfe2f18243feb996
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-19 17:08:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,42 @@ bot.on("message", async message => {
     })
 
   }
+
+  if(cmd === `@{prefix}setchannel`){
+
+    // Return Statements
+    if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires the Owner role**', 120000) // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
+    if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return func.embed(message.channel, '**Please mention a channel**\n > *~setChannel #channel*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
+
+    // Fetch the new channel they mentioned
+    let newChannel;
+    if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; // If they wrote the word none, it sets newChannel as empty.
+    else newChannel = message.mentions.channels.first().id; // If they actually mentioned a channel, it will set newChannel as that.
+
+    // Update Channel
+    db.updateText(`messageChannel_${message.guild.id}`, newChannel).then(i => {
+        func.embed(message.channel, `**Successfully updated logging channel to ${message.mentions.channels.first()}**`) // Finally, send in chat that they updated the channel.
+    })
+
+  }
+	
+  if(cmd === `${prefix}setwelcome`){
+
+    // Return Statements
+    if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires the Owner role**', 120000) // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
+    if (!args.join(" ") && args.join(" ").toUpperCase() !== 'NONE') return func.embed(message.channel, '**Please mention a channel**\n > *~setwelcome message*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
+
+    // Fetch the new channel they mentioned
+    let newMessage;
+    if (args.join(" ").toUpperCase() === 'NONE') newMessage = ''; // If they wrote the word none, it sets newMessage as empty.
+    else newMessage = args.join(" ").trim(); // If they didn't write none, set what they wrote as the message
+
+    // This will update the .text of the joinMessageDM_guildID object.
+    db.updateText(`joinMessage_${message.guild.id}`, newMessage).then(i => {
+        func.embed(message.channel, `**Successfully updated welcome text to:**\n > *${args.join(" ").trim()}*`) // Finally, send in chat that they updated the channel.
+    })
+
+  }
  
     
    
